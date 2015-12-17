bce87a0012a20a7413e2605c1437b8481129f9e7
Grant Colegate <blaknite@thelanbox.com.au>
2015-12-17 16:39:37 +1030

    Flashes are now created through JS

--- app/assets/javascripts/pure_admin/flash_messages.js
+++ app/assets/javascripts/pure_admin/flash_messages.js
@@ -1,7 +1,13 @@
 var PureAdmin = PureAdmin || {};
 
 PureAdmin.flash_messages = {
-  remove: function(element) {
-    $(element).closest('.flash').remove();
+  create: function(type, message) {
+    $('#flashes').prepend('<div class="flash ' + type + '">' + message + '</div>');
+    $('#flashes .flash:not(.bound-close)').addClass('bound-close')
+      .on('click', PureAdmin.flash_messages.remove);
+  },
+
+  remove: function(event) {
+    $(event.target).closest('.flash').remove();
   }
 }
