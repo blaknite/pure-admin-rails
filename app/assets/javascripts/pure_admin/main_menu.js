f6b6052f10f86419fc479604307e649e3072fa10
Grant Colegate <blaknite@thelanbox.com.au>
2015-12-17 21:48:45 +1030

    main menu now scrolls when swiping on touch devices

--- app/assets/javascripts/pure_admin/main_menu.js
+++ app/assets/javascripts/pure_admin/main_menu.js
@@ -27,26 +27,87 @@ $('document').ready(function() {
     var current_offset = parseInt(menu_list.attr('data-offset'));
     if ( isNaN(current_offset) ) current_offset = 0;
 
+    // how far we have moved this click
+    var new_offset = 0;
+
     if ( direction == 'left' ) {
       // we're scrolling left
-      if ( current_offset + offset > -offset ) {
+      new_offset = current_offset + offset;
+      if ( new_offset > -offset ) {
         menu_list.attr('data-offset', 0).css('left', '0px');
       } else {
-        menu_list.attr('data-offset', current_offset + offset).css('left', current_offset + offset + 'px');
+        menu_list.attr('data-offset', new_offset).css('left', new_offset + 'px');
       }
-
-      left_arrow.toggle(current_offset + offset <= -offset);
-      right_arrow.show();
     } else if ( direction == 'right' ) {
       // we're scrolling right
-      if ( current_offset - offset < -total_offset + offset ) {
+      new_offset = current_offset - offset;
+      if ( new_offset < -total_offset + offset ) {
         menu_list.attr('data-offset', -total_offset).css('left', '-' + total_offset + 'px');
       } else {
-        menu_list.attr('data-offset', current_offset - offset).css('left', current_offset - offset + 'px');
+        menu_list.attr('data-offset', new_offset).css('left', new_offset + 'px');
+      }
+    }
+
+    // toggle the navigation arrows
+    right_arrow.toggle(new_offset > -total_offset + offset);
+    left_arrow.toggle(new_offset < 0 - offset);
+  });
+
+  $('#main-menu').swipe({
+    swipeStatus: function(event, phase, direction, distance, duration, fingers) {
+      var element = $("#main-menu");
+
+      var menu_list = element.find('.pure-menu-list');
+      var left_arrow = element.find('.fade-left');
+      var right_arrow = element.find('.fade-right');
+
+      // do not scroll if list is shorter than document
+      if ( menu_list.width() < $(document).width() ) return;
+
+
+      // determine how far to scroll each click and the extremes of scrolling
+      var total_offset = menu_list.width() - $(document).width();
+
+      // how far we have already scrolled is stored on the list
+      var current_offset = parseInt(menu_list.attr('data-offset'));
+      if ( isNaN(current_offset) ) current_offset = 0;
+
+      // how far we have moved this swipe
+      var new_offset = menu_list.attr('data-temp-offset') || 0;
+
+      if ( phase == 'move') {
+        // disable animation while scrolling
+        menu_list.removeClass('with-animation');
+
+        if ( direction == 'left' ) {
+          // we're scrolling left
+          new_offset = current_offset - distance;
+          if ( new_offset < -total_offset ) {
+            menu_list.attr('data-offset', -total_offset).css('left', '-' + total_offset + 'px');
+          } else {
+            menu_list.attr('data-temp-offset', new_offset).css('left', new_offset + 'px');
+          }
+        } else if ( direction == 'right' ) {
+          // we're scrolling right
+          new_offset = current_offset + distance;
+          if ( new_offset > 0 ) {
+            menu_list.attr('data-offset', 0).css('left', '0px');
+          } else {
+            menu_list.attr('data-temp-offset', new_offset).css('left', new_offset + 'px');
+          }
+        }
       }
 
-      right_arrow.toggle(current_offset - offset >= -total_offset + offset);
-      left_arrow.show();
-    }
+      if ( phase == 'end' ) {
+        menu_list.attr('data-offset', new_offset);
+        menu_list.addClass('with-animation'); // enable animation
+      }
+
+      // toggle the navigation arrows
+      right_arrow.toggle(new_offset > -total_offset + 5);
+      left_arrow.toggle(new_offset < 0 - 5);
+    },
+    threshold: 10,
+    allowPageScroll: 'vertical'
   });
 });
