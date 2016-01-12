2a488e952072b14eb75e8d6eced0b3f4db69c881
Grant Colegate <blaknite@thelanbox.com.au>
2016-01-12 17:53:51 +1030

    combine details-panel and details-panel-body

--- app/helpers/pure_admin/details_panel_helper.rb
+++ app/helpers/pure_admin/details_panel_helper.rb
@@ -4,22 +4,11 @@ module PureAdmin::DetailsPanelHelper
   ##
   # Renders a "details panel" to the view.
   # @param title (String)
-  # @param options (Hash) a container for options to be passed to the panel and panel body.
-  # @param options[:panel_html] (Hash) all options that can be passed to content_tag are respected here.
-  # @param options[:body_html] (Hash) all options that can be passed to content_tag are respected here.
+  # @param options (Hash) all options that can be passed to content_tag are respected here.
   # @yield The contents of the details panel
-  def details_panel(options = nil, &block)
-    options ||= {}
-
-    panel_html = options.delete(:panel_html) || {}
-    panel_html[:class] = merge_html_classes('details-panel', panel_html[:class])
-
-    body_html = options.delete(:body_html) || {}
-    body_html[:class] = merge_html_classes('details-panel-body pure-g', body_html[:class])
-
-    content_tag(:div, panel_html) do
-      content_tag(:div, '', body_html, &block)
-    end
+  def details_panel(options = {}, &block)
+    options[:class] = merge_html_classes('details-panel', options[:class])
+    content_tag(:div, options, &block)
   end
 
   ##
@@ -28,16 +17,19 @@ module PureAdmin::DetailsPanelHelper
   # @param value (Any)
   # @param options (Hash) all options that can be passed to content_tag are respected here.
   # @yield The contents of the details panel item
-  def details_panel_item(label = nil, value = nil, options = nil, &block)
+  def details_panel_item(label, value = nil, options = nil, &block)
     options, value = value, nil if block_given?
+    options = options || {}
 
     label = label.to_s.titleize unless label.nil? || label.respond_to?(:titleize)
 
-    options ||= {}
-    options[:class] = merge_html_classes('details-item pure-u-1', options[:class])
+    item_html = options.delete(:item_html) || {}
+    item_html[:class] = merge_html_classes('details-panel-item pure-u-1', item_html[:class])
 
-    content_tag(:div, options) do
-      content_tag(:label, label) + (block_given? ? capture(&block) : value.to_s)
+    label_html = options.delete(:label_html) || {}
+
+    content_tag(:div, item_html) do
+      content_tag(:label, label, label_html) + (block_given? ? capture(&block) : value.to_s)
     end
   end
 
@@ -45,9 +37,8 @@ module PureAdmin::DetailsPanelHelper
   # Renders a "details_panel_controls" element to the view.
   # @param options (Hash) all options that can be passed to content_tag are respected here.
   # @yield The contents of the details panel controls
-  def details_panel_controls(options = nil, &block)
-    options ||= {}
-    options[:class] = merge_html_classes('details-panel-controls pure-u-1', options[:class])
+  def details_panel_controls(options = {}, &block)
+    options[:class] = merge_html_classes('details-panel-controls', options[:class])
     content_tag(:div, capture(&block), options)
   end
 end
