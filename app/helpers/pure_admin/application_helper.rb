77dc27fbf8fad5f5ba5e944f1e39ebbe7e3c4fab
Tobyn Dockerill <tobyn@mindvision.com.au>
2016-05-10 15:40:15 +0930

    Add boolean_label method
    This commit adds a helper method, `boolean_label`.
    It uses the tags defined in tags.scss.css to output appropriate HTML.

--- app/helpers/pure_admin/application_helper.rb
+++ app/helpers/pure_admin/application_helper.rb
@@ -9,4 +9,19 @@ module PureAdmin::ApplicationHelper
   def merge_html_classes(value1, value2)
     [value1, value2].flatten.compact
   end
+
+  ##
+  # Provides a "nice" looking boolean display using tags.
+  # @param (Boolean) value
+  # @return (String) HTML
+  def boolean_label(value)
+    return value if value.nil?
+
+    icon_class = 'fa'
+    icon_class << (value ? ' fa-check' : ' fa-times')
+    tag_class = 'tag'
+    tag_class << ' tag-green' if value
+
+    content_tag :span, content_tag(:i, '', class: icon_class), class: tag_class
+  end
 end
