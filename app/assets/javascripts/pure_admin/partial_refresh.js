21377ad6cc682d66c973b75ecc47df85d3984795
miegs3 <miegs3@hotmail.com>
2016-11-03 13:49:43 +1030

    Make Partial Refresh work with Turbolinks by not multiple initialising

--- app/assets/javascripts/pure_admin/partial_refresh.js
+++ app/assets/javascripts/pure_admin/partial_refresh.js
@@ -5,6 +5,9 @@ PureAdmin.partial_refresh = {
 
   init: function() {
     var matchElem = '.js-partial-refresh';
+    // Only initialise once, multiple times caused loading issues
+    if (window.partial_refresh_init == true)
+      return;
 
     /*
     * When the ajaxSend event is triggered on matchElem, show the loading indicator.
@@ -75,8 +78,13 @@ PureAdmin.partial_refresh = {
         elem.removeClass('loading');
       }
     }
+    window.partial_refresh_init = true;
   }
 };
 
-$(document).ready(PureAdmin.partial_refresh.init);
-$(document).on('turbolinks:load', PureAdmin.partial_refresh.init);
+// Init via Turbolinks load if that exists
+if (typeof(Turbolinks) == 'undefined') {
+  $(document).ready(PureAdmin.partial_refresh.init);
+} else {
+  $(document).on('turbolinks:load', PureAdmin.partial_refresh.init);
+}
